c8317809be2cff4c64ce533793292c4f7b8950eb
KRochaS <karinerocha.s@hotmail.com>
2023-11-16 20:34:06 -0300

    feat: Index Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,22 @@
-import { Button, Box } from '@chakra-ui/react';
+import { Box, Button } from '@chakra-ui/react';
 import { useMemo } from 'react';
 import { useInfiniteQuery } from 'react-query';
-
-import { Header } from '../components/Header';
 import { CardList } from '../components/CardList';
-import { api } from '../services/api';
-import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
+import { Header } from '../components/Header';
+import { Loading } from '../components/Loading';
+import { api } from '../services/api';
 
 export default function Home(): JSX.Element {
+  async function handleImages({ pageParam = 0 }): Promise<any> {
+    const { data } = await api.get('/api/images', {
+      params: {
+        after: pageParam,
+      },
+    });
+    return data;
+  }
+
   const {
     data,
     isLoading,
@@ -19,18 +27,26 @@ export default function Home(): JSX.Element {
   } = useInfiniteQuery(
     'images',
     // TODO AXIOS REQUEST WITH PARAM
-    ,
+    handleImages,
     // TODO GET AND RETURN NEXT PAGE PARAM
+    {
+      getNextPageParam: lastPage => lastPage.after || null,
+    }
   );
-
   const formattedData = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
+    return data?.pages.map(page => page.data).flat();
   }, [data]);
 
+  formattedData;
   // TODO RENDER LOADING SCREEN
-
+  if (isLoading) {
+    return <Loading />;
+  }
   // TODO RENDER ERROR SCREEN
-
+  if (isError) {
+    return <Error />;
+  }
   return (
     <>
       <Header />
@@ -38,6 +54,18 @@ export default function Home(): JSX.Element {
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
         {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+
+        {hasNextPage && (
+          <Button
+            my="40px"
+            onClick={() => fetchNextPage()}
+            isLoading={isFetchingNextPage}
+            loadingText="Carregando..."
+          >
+            {' '}
+            Carregar mais
+          </Button>
+        )}
       </Box>
     </>
   );
